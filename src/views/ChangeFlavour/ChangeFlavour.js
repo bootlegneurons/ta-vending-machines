53a5464d0efb3ae470e0a794b48704b286776312
bootlegneurons <bootlegneurons@gmail.com>
2020-04-12 17:55:49 +1000

    WIP: ProductSelection implementation. component skeletons

--- src/views/ChangeFlavour/ChangeFlavour.js
+++ src/views/ChangeFlavour/ChangeFlavour.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Row, Col, Input, Tooltip, Button } from 'antd';
 import { QuestionCircleOutlined } from '@ant-design/icons';
 import { StickyLayout, Header, Footer, Content } from 'components/layout';
@@ -6,41 +6,52 @@ import ProductSelection from './ProductSelection/ProductSelection';
 import FlavourLegend from './FlavourLegend/FlavourLegend';
 
 // TODO: Refactor Header/Footer content into new layout wrapper components
-const ChangeFlavour = () => (
-  <StickyLayout title="Change Flavour">
-    <Header>
-      <Row gutter={16} justify="space-between">
-        <Col>
-          <Input
-            placeholder="Search"
-            suffix={
-              <Tooltip title="Filter results by product name">
-                <QuestionCircleOutlined />
-              </Tooltip>
-            }
-          />
-        </Col>
-        <Col>
-          <FlavourLegend />
-        </Col>
-      </Row>
-    </Header>
-    <Content>
-      <ProductSelection />
-    </Content>
-    <Footer>
-      <Row gutter={8} justify="end">
-        <Col>
-          <Button size="large">Cancel</Button>
-        </Col>
-        <Col>
-          <Button size="large" type="primary">
-            Save
-          </Button>
-        </Col>
-      </Row>
-    </Footer>
-  </StickyLayout>
-);
+const ChangeFlavour = () => {
+  const [search, setSearch] = useState('');
+  const [newProductId, setNewProductId] = useState('');
+
+  return (
+    <StickyLayout title="Change Flavour">
+      <Header>
+        <Row gutter={16} justify="space-between">
+          <Col flex={1}>
+            <Input
+              onChange={({ target: { value } }) => setSearch(value)}
+              placeholder="Search"
+              suffix={
+                <Tooltip title="Filter results by product name">
+                  <QuestionCircleOutlined />
+                </Tooltip>
+              }
+              value={search}
+            />
+          </Col>
+          <Col flex={4}>
+            <FlavourLegend />
+          </Col>
+        </Row>
+      </Header>
+      <Content>
+        <ProductSelection
+          onProductSelected={id => setNewProductId(id)}
+          search={search}
+          value={newProductId}
+        />
+      </Content>
+      <Footer>
+        <Row gutter={8} justify="end">
+          <Col>
+            <Button size="large">Cancel</Button>
+          </Col>
+          <Col>
+            <Button disabled={!!newProductId} size="large" type="primary">
+              Save
+            </Button>
+          </Col>
+        </Row>
+      </Footer>
+    </StickyLayout>
+  );
+};
 
 export default ChangeFlavour;
--- src/views/ChangeFlavour/ProductSelection/ProductSelection.js
+++ src/views/ChangeFlavour/ProductSelection/ProductSelection.js
@@ -1,5 +1,71 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Row, Col } from 'antd';
+import SelectedProducts from './SelectedProducts';
+import OtherProducts from './OtherProducts';
 
-const ProductSelection = () => <p>Product Selection placeholder</p>;
+// TODO: replace hardcoded data with provided data source
+const currentProducts = [
+  {
+    display_category: 0,
+    product_code: '133129',
+    product_name: '16Z CN 24LS_MON NRG',
+    type_name: 'BEVERAGE',
+    average_sales: 1.24275,
+    price: 275,
+    subsidy_price: 0,
+    expected_price: 275,
+    revenue_bar_width: 28.2876883513,
+    is_known: true,
+    is_predicted: false,
+  },
+  {
+    display_category: 0,
+    product_code: '114756',
+    product_name: '20Z PT 24LS_FANTA ORG',
+    type_name: 'BEVERAGE',
+    average_sales: 2.05903,
+    price: 175,
+    subsidy_price: 0,
+    expected_price: 175,
+    revenue_bar_width: 28.1207961115,
+    is_known: true,
+    is_predicted: false,
+  },
+];
+
+const ProductSelection = ({ onProductSelected, search, value }) => {
+  const [currentProduct, setCurrentProduct] = useState('');
+
+  // TODO: on currentProduct or search change, refetch other products
+  // TODO: debounce fn call ? prevent race conditions
+  useEffect(() => {}, [currentProduct, search]);
+
+  return (
+    <Row>
+      <Col flex={1}>
+        <SelectedProducts
+          items={currentProducts}
+          onChange={id => setCurrentProduct(id)}
+          value={currentProduct}
+        />
+      </Col>
+      <Col flex={4}>
+        <OtherProducts
+          items={[]}
+          onChange={onProductSelected}
+          selectedId={null}
+          value={value}
+        />
+      </Col>
+    </Row>
+  );
+};
+
+ProductSelection.propTypes = {
+  onProductSelected: PropTypes.func,
+  search: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+};
 
 export default ProductSelection;
